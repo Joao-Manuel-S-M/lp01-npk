35c032aab8a1f815055fa3705c9043e17c54ef66
João Manuel de Souza Marques <93559636+Joao-Manuel-S-M@users.noreply.github.com>
2026-04-02 15:16:42 +0000

    update

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -3,12 +3,12 @@ const VideoPlayer = () => {
 		<div className="relative w-full max-w-[400px] mx-auto">
 			<div className="relative bg-black pt-[178.22%]">
 				<iframe
-					src="https://player.vimeo.com/video/1176758204?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&muted=0&controls=0"
+					src="https://player.vimeo.com/video/1179604624?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
 					className="absolute top-0 left-0 w-full h-full"
 					frameBorder="0"
 					allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
 					referrerPolicy="strict-origin-when-cross-origin"
-					title="NPK Natrual"
+					title="NPK_Natrual"
 				/>
 				<div className="absolute inset-0 z-10" aria-hidden="true" />
 			</div>
